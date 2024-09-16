964e3e9f439e895036069c735c9aea9451b1d573
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-09-16 08:44:26 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -92,7 +92,7 @@ export default function Home(props) {
           </p>
           <p>
             Sign up for an 👨‍🎤 account, ✍️ write posts; 💖 hearts & 💬 comments other's posts. All public content is server-rendered and
-            seo optimized. <center>( 🛡️<u><a href="/raselshikdar/privacy-policy" aria-lebel="Privacy Policy"> Privacy Policy</a></u> & 📃<u><a href="/raselshikdar/terms-of-service" aria-lebel="Terms of Service"> T.O.S</a></u> )</center>
+            seo optimized. (🛡️<u><a href="/raselshikdar/privacy-policy" aria-lebel="Privacy Policy"> Privacy Policy</a></u> & 📃<u><a href="/raselshikdar/terms-of-service" aria-lebel="Terms of Service"> T.O.S</a></u>)
           </p>
           <span className="exit-hero" onClick={exitHero}>
             <abbr title="close">
