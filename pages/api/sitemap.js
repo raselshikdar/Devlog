47ba8b391a21cf225b9979c0452155f4988fe0f4
Rasel Shikdar <imrasel597@gmail.com>
2025-01-29 16:52:30 +0600

    Update sitemap.js

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,18 +1,18 @@
-import { SitemapStream, streamToPromise } from 'sitemap'
-import { Readable } from 'stream'
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { Readable } from 'stream';
 
 export default async (req, res) => {
   const links = [
     { url: '/', changefreq: 'daily', priority: 1.0 },
     { url: '/about', changefreq: 'monthly', priority: 0.8 },
     // Add more URLs as needed
-  ]
+  ];
 
-  const stream = new SitemapStream({ hostname: 'https://devlog.rweb.site' })
-  const xmlString = await streamToPromise(Readable.from(links).pipe(stream)).then(data => data.toString())
+  const stream = new SitemapStream({ hostname: 'https://devlog.rweb.site' });
+  const xmlString = await streamToPromise(Readable.from(links).pipe(stream)).then(data => data.toString());
 
   res.writeHead(200, {
     'Content-Type': 'application/xml'
-  })
-  res.end(xmlString)
-}
+  });
+  res.end(xmlString);
+};
