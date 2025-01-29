18982face311455827be172a5d882a739b244283
Rasel Shikdar <imrasel597@gmail.com>
2025-01-29 15:15:17 +0600

    Update index.js

--- components/PostFeed/index.js
+++ components/PostFeed/index.js
@@ -1,30 +1,32 @@
+import { formatDistanceToNow } from "date-fns";
 import Link from "next/link";
 import s from "./PostFeed.module.css";
 
 export default function PostFeed({ posts, admin }) {
   return (
     <div className={s.container}>
-      {posts
-        ? posts.map(post => (
-            <PostItem post={post} key={post.slug} admin={admin} />
-          ))
-        : null}
+      {posts?.map(post => (
+        <PostItem post={post} key={post.slug} admin={admin} />
+      ))}
     </div>
   );
 }
 
 function PostItem({ post, admin = false }) {
-  // Naive method to calc word count and read time
   const wordCount = post?.content.trim().split(/\s+/g).length;
   const minutesToRead = (wordCount / 100 + 1).toFixed(0);
+  const daysAgo = formatDistanceToNow(new Date(post.createdAt), { addSuffix: true });
 
   return (
     <div className="card">
-      <Link href={`/${post.username}`}>
-        <a>
-          <strong>By @{post.username}</strong>
-        </a>
-      </Link>
+      <div className={s.postHeader}>
+        <Link href={`/${post.username}`}>
+          <a>
+            <strong>By @{post.username}</strong>
+          </a>
+        </Link>
+        <span className={s.daysAgo}>{daysAgo}</span> {/* Days ago added here */}
+      </div>
 
       <Link href={`/${post.username}/${post.slug}`}>
         <h2>
@@ -39,7 +41,6 @@ function PostItem({ post, admin = false }) {
         <span className="push-left">💖 {post.heartCount || 0} Hearts</span>
       </footer>
 
-      {/* If admin view, show extra controls for user */}
       {admin && (
         <>
           <Link href={`/admin/${post.slug}`}>
