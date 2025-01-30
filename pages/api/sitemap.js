877224e8909deaec5bba02efa127b9033cf0c5b9
Rasel Shikdar <imrasel597@gmail.com>
2025-01-30 23:15:46 +0600

    Update sitemap.js

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -6,7 +6,16 @@ export default async (req, res) => {
 
   const links = [
     { url: '/', changefreq: 'daily', priority: 1.0 },
-    { url: '/about', changefreq: 'monthly', priority: 0.8 }
+    { url: '/rasel/about-us', changefreq: 'monthly', priority: 0.8 },
+  { url: '/rasel/contact-us', changefreq: 'monthly', priority: 0.8 },
+  { url: '/rasel/sponsor-us', changefreq: 'monthly', priority: 0.8 },
+  { url: '/rasel/documentations', changefreq: 'monthly', priority: 0.8 },
+  { url: '/rasel/terms-of-use', changefreq: 'monthly', priority: 0.8 },
+  { url: '/rasel/privacy-policy', changefreq: 'monthly', priority: 0.8 },
+  { url: '/rasel/how-to-write-blog-posts-in-devlog', changefreq: 'monthly', priority: 0.8 },
+  { url: '/rasel/how-to-write-blog-posts-in-devlog-bangla-tutorial', changefreq: 'monthly', priority: 0.8 },
+  { url: '/rasel/new-feature-multi-language-supports-for-codes', changefreq: 'monthly', priority: 0.8 },
+  { url: '/wasik/this-is-an-awesome-blog', changefreq: 'monthly', priority: 0.8 }
   ];
 
   const stream = new SitemapStream({ hostname: SITE_URL });
