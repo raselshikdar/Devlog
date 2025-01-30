813dd49c4d1802e5cb3f924e43ccc2cf67e20082
Rasel Shikdar <imrasel597@gmail.com>
2025-01-30 23:05:37 +0600

    Update generate-sitemap.js

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -8,6 +8,14 @@ const urls = [
   { url: '/', changefreq: 'daily', priority: 1.0 },
   { url: '/rasel/about-us', changefreq: 'monthly', priority: 0.8 },
   { url: '/rasel/contact-us', changefreq: 'monthly', priority: 0.8 },
+  { url: '/rasel/sponsor-us', changefreq: 'monthly', priority: 0.8 },
+  { url: '/rasel/documentations', changefreq: 'monthly', priority: 0.8 },
+  { url: '/rasel/terms-of-use', changefreq: 'monthly', priority: 0.8 },
+  { url: '/rasel/privacy-policy', changefreq: 'monthly', priority: 0.8 },
+  { url: '/rasel/how-to-write-blog-posts-in-devlog', changefreq: 'monthly', priority: 0.8 },
+  { url: '/rasel/how-to-write-blog-posts-in-devlog-bangla-tutorial', changefreq: 'monthly', priority: 0.8 },
+  { url: '/rasel/new-feature-multi-language-supports-for-codes', changefreq: 'monthly', priority: 0.8 },
+  { url: '/wasik/this-is-an-awesome-blog', changefreq: 'monthly', priority: 0.8 },
   // Add other URLs here
 ];
 
