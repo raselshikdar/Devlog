02a4f87171c7bc73ccca9daf1ee4e8f233c066be
Rasel Shikdar <imrasel597@gmail.com>
2025-01-31 11:41:13 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -81,7 +81,7 @@ export default function Home(props) {
     <main>
       <Metatags
         title="Devlog | Home"
-        description="Empowering developers to share, learn, and grow—one blog at a time."
+        description="Devlog is a dynamic platform where developers can share insights, codes, and ideas. Engage with the latest articles, exchange thoughts, and grow together with a thriving tech community."
       />
       {!exitedHero && (
         <div className="card card-info hero">
