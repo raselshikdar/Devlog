d1887e638266f948d9d30f085644df26c80eec9d
Rasel Shikdar <imrasel597@gmail.com>
2025-01-31 14:20:23 +0600

    Update index.js

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -6,15 +6,14 @@ export default function Footer() {
     <footer className={s.footer}>
       <div className={s.footerContent}>
         <div className={s.links}>
-          <Link href="/about">About Us</Link>
-          <Link href="/contact">Contact</Link>
-          <Link href="/terms">Terms of Use</Link>
-          <Link href="/privacy">Privacy Policy</Link>
-          <Link href="/documentations">Documentation</Link>
+          <Link href="/rasel/about-us">About Us</Link>
+          <Link href="/rasel/contact-us">Contact</Link>
+          <Link href="/rasel/terms-of-use">Terms of Use</Link>
+          <Link href="/rasel/privacy-policy">Privacy Policy</Link>
+          <Link href="/rasel/documentations">Documentation</Link>
         </div>
         <div className={s.copy}>
-          © {new Date().getFullYear()} Devlog. All rights reserved.<br />
-          Built with Next.js & Firebase. Deployed on Vercel.
+          © {new Date().getFullYear()} <Link href="/">Devlog</Link>. All rights reserved.
         </div>
       </div>
     </footer>
