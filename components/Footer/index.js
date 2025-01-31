6ec3b44da5642d2925cad6e37e37b5ffe491a685
Rasel Shikdar <imrasel597@gmail.com>
2025-01-31 15:10:32 +0600

    Update index.js

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -6,11 +6,10 @@ export default function Footer() {
     <footer className={s.footer}>
       <div className={s.footerContent}>
         <div className={s.links}>
-          <Link href="/rasel/about-us">About</Link>
-          <Link href="/rasel/privacy-policy">Privacy</Link>
-          <Link href="/rasel/terms-of-use">Terms</Link>
-          <Link href="/rasel/sponsor-us">Advertise</Link>
-          <Link href="/rasel/contact-us">Contact</Link>
+          <Link href="/rasel/about-us">About Us</Link>
+          <Link href="/rasel/terms-of-use">Terms of Use</Link>
+          <Link href="/rasel/contact-us">Contact Us</Link>
+          <Link href="/rasel/privacy-policy">Privacy Policy</Link>
           <Link href="/rasel/documentations">Documentations</Link>
         </div>
       </div>
