60f98bcdc135134727780c2e52966646a624f676
Rasel Shikdar <imrasel597@gmail.com>
2025-02-03 21:32:21 +0600

    Create next-sitemap.js

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteUrl: 'https://devlog.rweb.site',
+  generateRobotsTxt: true,  // Generate robots.txt
+  sitemapSize: 5000,  // Max number of URLs per sitemap
+  exclude: ['/404', '/admin'],  // Exclude specific pages from sitemap
+};
