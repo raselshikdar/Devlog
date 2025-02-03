3be7d59cb6dbcef3a5ce137316f3d22f77a4437d
Rasel Shikdar <imrasel597@gmail.com>
2025-02-03 21:46:16 +0600

    Update next-sitemap.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,11 +1,14 @@
 module.exports = {
   siteUrl: 'https://devlog.rweb.site', // Your website address
   generateRobotsTxt: true, // Enable robots.txt generation
-  sitemapSize: 70000, // Adjust the size limit (e.g., up to 70,000 URLs)
+  sitemapSize: 70000, // Adjust the size limit (up to 70,000 URLs)
   outDir: './public', // The output directory for the sitemap
   changefreq: 'daily', // Frequency for URL changes
   priority: 0.7, // Default priority for URLs
-  generateIndexSitemap: false, // This will prevent the index sitemap (sitemap.xml) from being generated as a list of URLs
+  generateIndexSitemap: false, // Disable generation of sitemap index (sitemap-0.xml, sitemap-1.xml)
+  additionalSitemaps: [
+    'https://devlog.rweb.site/sitemap.xml', // Ensure this points to your main sitemap
+  ],
   transform: async (config, url) => {
     return {
       loc: url,
