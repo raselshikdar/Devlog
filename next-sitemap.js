99cd7129b993b493c0603bdbf9a1a61101450ac7
Rasel Shikdar <imrasel597@gmail.com>
2025-02-03 21:03:52 +0600

    Delete next-sitemap.js

--- next-sitemap.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  siteUrl: 'https://devlog.rweb.site',
-  generateRobotsTxt: true, // (optional) Generate a robots.txt file
-  sitemapSize: 7000,
-  outDir: './public', // Ensures sitemap is in the 'public' folder
-  filename: 'sitemap.xml', // This ensures the sitemap file is named 'sitemap.xml'
-  changefreq: 'daily', // (optional) Set how often pages are likely to change
-  priority: 0.7, // (optional) Set priority for pages
-}
