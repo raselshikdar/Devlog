b31fff53666665eb35785f8a12e35b1b6f5b0c37
Rasel Shikdar <imrasel597@gmail.com>
2025-02-03 20:29:57 +0600

    Create next-sitemap.js

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+// next-sitemap.js
+module.exports = {
+  siteUrl: 'https://devlog.rweb.site', // Your website URL
+  generateRobotsTxt: true, // Optional: Generate robots.txt file automatically
+  sitemapSize: 7000, // Optional: Control the number of URLs per sitemap
+  exclude: ['/admin/**'], // Optional: Exclude certain pages or routes
+  // You can further customize your sitemap as needed
+}
