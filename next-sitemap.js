93f94525a7a90ec15bda975d65bd5707fb0e9cb5
Rasel Shikdar <imrasel597@gmail.com>
2025-02-03 20:47:58 +0600

    Update next-sitemap.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,8 +1,9 @@
-// next-sitemap.js
 module.exports = {
-  siteUrl: 'https://devlog.rweb.site', // Your website URL
-  generateRobotsTxt: true, // Optional: Generate robots.txt file automatically
-  sitemapSize: 7000, // Optional: Control the number of URLs per sitemap
-  exclude: ['/admin/**'], // Optional: Exclude certain pages or routes
-  // You can further customize your sitemap as needed
+  siteUrl: 'https://devlog.rweb.site',
+  generateRobotsTxt: true, // (optional) Generate a robots.txt file
+  sitemapSize: 7000,
+  outDir: './public', // Ensures sitemap is in the 'public' folder
+  filename: 'sitemap.xml', // This ensures the sitemap file is named 'sitemap.xml'
+  changefreq: 'daily', // (optional) Set how often pages are likely to change
+  priority: 0.7, // (optional) Set priority for pages
 }
