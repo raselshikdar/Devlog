faa95463e75d3b8f69d46e9ea460dfef90338588
Rasel Shikdar <imrasel597@gmail.com>
2025-02-06 20:05:34 +0600

    Update index.js

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -2,6 +2,8 @@ import { getUserWithUsername, postToJSON } from "../../lib/firebase";
 import UserProfile from "../../components/UserProfile";
 import Metatags from "../../components/Metatags";
 import PostFeed from "../../components/PostFeed";
+import Link from "next/link"; // Import Link for navigation
+import { auth } from "../../lib/firebase"; // Import Firebase auth
 import {
   getDocs,
   query as firebaseQuery,
@@ -14,6 +16,7 @@ import {
 export async function getServerSideProps({ query }) {
   const { username } = query;
 
+  // Fetch user data from Firestore
   const userDoc = await getUserWithUsername(username);
 
   // If no user, short circuit to 404 page
@@ -35,7 +38,6 @@ export async function getServerSideProps({ query }) {
       orderBy("createdAt", "desc"),
       limit(20)
     );
-
     posts = (await getDocs(postsQuery)).docs.map(postToJSON);
   }
 
@@ -47,11 +49,27 @@ export async function getServerSideProps({ query }) {
 export default function UserProfilePage({ user, posts }) {
   return (
     <main>
+      {/* Meta tags for SEO */}
       <Metatags
         title={user.username}
         description={`${user.username}'s public profile`}
       />
+
+      {/* User Profile Component */}
       <UserProfile user={user} />
+
+      {/* Add a link to the Edit Profile page if the current user is viewing their own profile */}
+      {auth.currentUser?.uid === user.id && (
+        <div style={{ marginTop: "1rem" }}>
+          <Link href="/profile/edit">
+            <a style={{ textDecoration: "underline", color: "blue" }}>
+              Edit Profile
+            </a>
+          </Link>
+        </div>
+      )}
+
+      {/* Display the user's posts */}
       <PostFeed posts={posts} />
     </main>
   );
