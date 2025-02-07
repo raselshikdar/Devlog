162136ea551dc003d843bb79c6d5c31dc970e5eb
Rasel Shikdar <imrasel597@gmail.com>
2025-02-08 01:29:51 +0600

    Update index.js

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,15 +1,17 @@
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { UserContext } from "../../lib/context/userContext";
 import { ThemeContext } from "../../lib/context/themeContext";
 import { ImSun } from "react-icons/im";
 import { BiMoon } from "react-icons/bi";
+import { FaSearch } from "react-icons/fa";
 import s from "./Navbar.module.css";
+import Search from "../Search/Search"; // Import the search component
 
-// Top navbar
 export default function Navbar() {
   const { user, username } = useContext(UserContext);
   const { theme, changeTheme } = useContext(ThemeContext);
+  const [showSearch, setShowSearch] = useState(false);
 
   const handleThemeChange = () => {
     changeTheme(theme === "light" ? "dark" : "light");
@@ -24,12 +26,20 @@ export default function Navbar() {
           </Link>
         </li>
 
+        <li>
+          <div onClick={() => setShowSearch(!showSearch)} className={s.searchIcon}>
+            <FaSearch />
+          </div>
+        </li>
+
+        {showSearch && <Search />} {/* Show Search when clicked */}
+
         <li className="push-left">
           <div onClick={handleThemeChange} className={s.switch}>
             {theme === "light" ? <BiMoon /> : <ImSun />}
           </div>
         </li>
-        {/* user is signed-in and has username */}
+
         {username && (
           <>
             <li>
@@ -50,7 +60,6 @@ export default function Navbar() {
           </>
         )}
 
-        {/* user is not signed OR has not created username */}
         {!username && (
           <li>
             <Link href="/signin">
