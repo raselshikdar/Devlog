0d63e025d3db21cda9d7de613569036a345d1b78
Rasel Shikdar <imrasel597@gmail.com>
2025-02-11 00:12:05 +0600

    Update firebaseadmin.js

--- pages/api/firebaseadmin.js
+++ pages/api/firebaseadmin.js
@@ -3,11 +3,14 @@ import admin from 'firebase-admin';
 if (!admin.apps.length) {
   // Initialize Firebase Admin SDK if it's not already initialized
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(), // Use your credentials here
-    databaseURL: process.env.FIREBASE_DATABASE_URL, // optional, if using Realtime Database
+    credential: admin.credential.cert({
+      projectId: process.env.NEXT_PUBLIC_FIRE_PROJECT_ID,
+      privateKey: process.env.NEXT_PUBLIC_FIRE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.NEXT_PUBLIC_FIRE_CLIENT_EMAIL,
+    }),
   });
 } else {
-  admin.app(); // If already initialized, use existing instance
+  admin.app(); // If already initialized, use the existing instance
 }
 
 export default async function handler(req, res) {
